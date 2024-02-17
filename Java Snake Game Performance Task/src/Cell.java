b7f38cf6199b2f54af1d4a6af8e526aa36b565fc
CristianGo165 <137358764+CristianGo165@users.noreply.github.com>
2024-02-16 22:17:35 -0600

    Add files via upload

---
+++ Java Snake Game Performance Task/src/Cell.java
@@ -0,0 +1,29 @@
+
+
+public class Cell {
+
+    private final int row;
+    private final int col;
+
+    public Cell(int row, int col){
+        this.row = row;
+        this.col = col;
+    }
+
+    public int GetRowCell(){
+        return row;
+    }
+
+    public int GetColCell(){
+        return col;
+    }
+
+    private CellType cellType;
+
+    public CellType getTypeCell(){
+        return cellType;
+    }
+    public void SetTypeCell(CellType cellType){
+        this.cellType = cellType;
+    }
+}
---
+++ Java Snake Game Performance Task/src/CellType.java
@@ -0,0 +1,3 @@
+public enum CellType {
+    SPACE, FOOD, TRAP, SNAKE_BODY;
+}
